24ba1e0999112e4ef722e0761e35de55f0b110b3
p1x <p1xel.com@gmail.com>
2020-04-19 23:57:30 +0300

    Add basic commands and some colorizaiton.

---
+++ QSPNet.Interpreter/ColoredConsole.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace QSPNet.Interpreter {
+    public static class ColoredConsole {
+        public static void WriteLine(string text) {
+            foreach (var part in text.Split('#')) {
+                if (part.ToUpper() == "RESET")
+                    Console.ResetColor();
+                else if (Enum.TryParse<ConsoleColor>(part, true, out var color))
+                    Console.ForegroundColor = color;
+                else
+                    Console.Write(part);
+            }
+            Console.WriteLine();
+        }
+    }
+}
--- QSPNet.Interpreter/Program.cs
+++ QSPNet.Interpreter/Program.cs
@@ -1,24 +1,44 @@
-﻿using System;
+using System;
 
 namespace QSPNet.Interpreter {
     static class Program {
+        private static ReplOptions _replOptions = ReplOptions.None;
+        
         private static void Main(string[] args) {
             while (true) {
                 Console.Write("> ");
 
                 var line = Console.ReadLine();
-                switch (line) {
-                    case "/exit":
-                        return;
-                    case "/cls":
-                        Console.Clear();
-                        break;
-                    default:
-                        Process(line);
-                        break;
+                if (line == null)
+                    continue;
+                
+                if (line.StartsWith('/')) {
+                    switch (line.ToUpperInvariant()) {
+                        case "/Q":
+                        case "/EXIT":
+                            return;
+                        case "/CLS":
+                            Console.Clear();
+                            break;
+                        case "/LEX":
+                        case "/SHOWLEX":
+                            _replOptions = SwitchFlag(ReplOptions.PrintLexedTokens);
+                            var printLexString = _replOptions.HasFlag(ReplOptions.PrintLexedTokens)
+                                ? $"#{ConsoleColor.Green}#enabled#RESET#"
+                                : $"#{ConsoleColor.Red}#disabled#RESET#";
+                            ColoredConsole.WriteLine($"Printing lexed tokens {printLexString}.");
+                            break;
+                        default:
+                            Console.Error.WriteLine($"Unknown command: {line}.");
+                            break;
+                    }    
+                } else {
+                    Process(line);
                 }
             }
         }
+
+        private static ReplOptions SwitchFlag(ReplOptions flag) => (_replOptions & flag) != 0 ? _replOptions & ~flag : _replOptions ^ flag;
 
         private static void Process(string line) {
             Console.WriteLine(line);
