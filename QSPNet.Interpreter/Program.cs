bb045012e052b3f90c42c9223502b94365b4bc5a
p1x <p1xel.com@gmail.com>
2020-04-19 23:04:57 +0300

    Simple echo REPL

--- QSPNet.Interpreter/Program.cs
+++ QSPNet.Interpreter/Program.cs
@@ -1,9 +1,27 @@
 ﻿using System;
 
 namespace QSPNet.Interpreter {
-    class Program {
-        static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+    static class Program {
+        private static void Main(string[] args) {
+            while (true) {
+                Console.Write("> ");
+
+                var line = Console.ReadLine();
+                switch (line) {
+                    case "/exit":
+                        return;
+                    case "/cls":
+                        Console.Clear();
+                        break;
+                    default:
+                        Process(line);
+                        break;
+                }
+            }
+        }
+
+        private static void Process(string line) {
+            Console.WriteLine(line);
         }
     }
 }
