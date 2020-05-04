300067e67ba684e18331342ff1c51524233d87cc
p1x <p1xel.com@gmail.com>
2020-05-04 12:14:18 +0300

    Extract parser base class.

--- QSPNet.Interpreter/Parser/Parser.cs
+++ QSPNet.Interpreter/Parser/Parser.cs
@@ -1,33 +1,14 @@
 ﻿namespace QSPNet.Interpreter {
-    public class Parser {
-        private readonly string _text;
-        private readonly Lexer _lexer;
-        
-        private readonly ParserDiagnosticBag _diagnostics = new ParserDiagnosticBag();
+    public class Parser : ParserBase {
+        public Parser(string text) : base(text) { }
 
-        private SyntaxToken _current;
-        private SyntaxToken _next;
+        protected override ExpressionSyntax ParseCore() => ParseExpression();
 
-        public Parser(string text) {
-            _text = text;
-            _lexer = new Lexer(text);
-            _current = FilterNext(_lexer);
-            _next = FilterNext(_lexer);
-        }
-
-        public (SyntaxTree syntaxTree, DiagnosticBag diagnostics) Parse() {
-            var expression = ParseExpression();
-            var endOfFileToken = Match(SyntaxKind.EndOfFileToken);
-            var syntaxTree = new SyntaxTree(_text, expression, endOfFileToken);
-            var diagnostics = _lexer.GetDiagnostics().With(_diagnostics);
-            return (syntaxTree, diagnostics);
-        }
-
-        private ExpressionSyntax ParseExpression() {
+        private ExpressionSyntax ParseExpression(int parentPrecedence = 0) {
             var left = ParsePrimaryExpression();
 
-            while (_current.Kind == SyntaxKind.PlusToken ||
-                   _current.Kind == SyntaxKind.MinusToken) {
+            while (Current.Kind == SyntaxKind.PlusToken ||
+                   Current.Kind == SyntaxKind.MinusToken) {
                 var operatorToken = Next();
                 var right = ParsePrimaryExpression();
                 left = new BinaryExpressionSyntax(left, operatorToken, right);
@@ -35,35 +16,10 @@
 
             return left;
         }
-
+        
         private ExpressionSyntax ParsePrimaryExpression() {
             var token = Match(SyntaxKind.NumberToken);
             return new NumberExpressionSyntax(token);
-        }
-
-        private static SyntaxToken FilterNext(Lexer lexer) {
-            SyntaxToken token;
-            do
-                token = lexer.Next();
-            while (token.Kind == SyntaxKind.WhiteSpaceToken ||
-                   token.Kind == SyntaxKind.UnknownToken); 
-
-            return token;
-        }
-
-        private SyntaxToken Next() {
-            var current = _current;
-            _current = _next;
-            _next = FilterNext(_lexer);
-            return current;
-        }
-
-        private SyntaxToken Match(SyntaxKind kind) {
-            if (_current.Kind == kind)
-                return Next();
-
-            _diagnostics.ReportUnexpectedToken(kind, _current);
-            return SyntaxToken.Manufacture(kind, _current.Position);
         }
     }
 }
---
+++ QSPNet.Interpreter/Parser/ParserBase.cs
@@ -0,0 +1,55 @@
+﻿namespace QSPNet.Interpreter {
+    public abstract class ParserBase {
+        private readonly string _text;
+        private readonly Lexer _lexer;
+        
+        private readonly ParserDiagnosticBag _diagnostics = new ParserDiagnosticBag();
+
+        private SyntaxToken _current;
+        private SyntaxToken _next;
+
+        protected ParserBase(string text) {
+            _text = text;
+            _lexer = new Lexer(text);
+            _current = FilterNext(_lexer);
+            _next = FilterNext(_lexer);
+        }
+        
+        public (SyntaxTree syntaxTree, DiagnosticBag diagnostics) Parse() {
+            var expression = ParseCore();
+            var endOfFileToken = Match(SyntaxKind.EndOfFileToken);
+            var syntaxTree = new SyntaxTree(_text, expression, endOfFileToken);
+            var diagnostics = _lexer.GetDiagnostics().With(_diagnostics);
+            return (syntaxTree, diagnostics);
+        }
+
+        protected abstract ExpressionSyntax ParseCore();
+
+        private static SyntaxToken FilterNext(Lexer lexer) {
+            SyntaxToken token;
+            do
+                token = lexer.Next();
+            while (token.Kind == SyntaxKind.WhiteSpaceToken ||
+                   token.Kind == SyntaxKind.UnknownToken); 
+
+            return token;
+        }
+
+        protected SyntaxToken Next() {
+            var current = _current;
+            _current = _next;
+            _next = FilterNext(_lexer);
+            return current;
+        }
+
+        protected SyntaxToken Match(SyntaxKind kind) {
+            if (_current.Kind == kind)
+                return Next();
+
+            _diagnostics.ReportUnexpectedToken(kind, _current);
+            return SyntaxToken.Manufacture(kind, _current.Position);
+        }
+
+        protected SyntaxToken Current => _current;
+    }
+}
